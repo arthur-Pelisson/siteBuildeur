2ca91ae03a54b2ab82c65a20e49c182482d3d85a
luci <pelisson.arthur@gmail.com>
2024-07-12 08:27:14 +0200

    change seed

--- server/src/Components/user/user.seed.ts
+++ server/src/Components/user/user.seed.ts
@@ -1,7 +1,7 @@
 import UserTest from "../test/userTest";
 
 async function main() {
-    const UserAdmin = await UserTest.createTestUser("mail@seed.admin.com", "!Azerty123", "jhon", "Doe", true, "ADMIN");
+    const UserAdmin = await UserTest.createTestUser("mail@seed.admin.com", "!Adminexample123", "jhon", "Doe", true, "ADMIN");
     const UserUser = await UserTest.createTestUser("user@seed.user.com", "!Userexample123", "User", "Model", true, "USER");
 } 
 main();
